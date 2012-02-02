a37db6b842f98cdb00afa6e7f89e6895eae0ebd5
Michael Dippery <michael@monkey-robot.com>
2012-02-02 15:48:01 -0500

    Automatically page output when listing comments for a subreddit
    Eventually I'd like to control this behavior with a flag or an
    environment variable, but for now it's automatic.

---
+++ lib/usaidwat/utils.rb
@@ -0,0 +1,25 @@
+# Inspired by: http://nex-3.com/posts/73-git-style-automatic-paging-in-ruby
+def run_pager
+  return if RUBY_PLATFORM =~ /win32/
+  return unless STDOUT.tty?
+  
+  read, write = IO.pipe
+  
+  unless Kernel.fork
+    STDOUT.reopen(write)
+    STDERR.reopen(write) if STDERR.tty?
+    read.close
+    write.close
+    return
+  end
+  
+  STDIN.reopen(read)
+  read.close
+  write.close
+  
+  #ENV['LESS'] = 'FSRX'
+  
+  Kernel.select [STDIN]
+  pager = ENV['PAGER'] || 'less'
+  exec pager rescue exec '/bin/sh', '-c', pager
+end
