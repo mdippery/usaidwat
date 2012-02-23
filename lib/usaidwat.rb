df0fef8a91c884f0fdb000ba771f50438ac236b8
Michael Dippery <michael@monkey-robot.com>
2012-02-22 22:52:30 -0500

    Remove an unused method

--- lib/usaidwat.rb
+++ lib/usaidwat.rb
@@ -4,11 +4,4 @@ require 'usaidwat/reddit'
 
 module USaidWat
   BASE_CACHE_DIR = File.join File.expand_path('~'), '.usaidwat'
-  
-  module Commands extend self
-    def damn(message, exit_code=1)
-      $stderr.puts message
-      exit exit_code
-    end
-  end
 end
