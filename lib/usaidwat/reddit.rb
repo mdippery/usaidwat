18fa11eb5ff9b4e64b2a37b165eed806554f539b
Michael Dippery <michael@monkey-robot.com>
2012-06-04 16:34:34 -0400

    Sort subreddit counts alphabetically

--- lib/usaidwat/reddit.rb
+++ lib/usaidwat/reddit.rb
@@ -78,7 +78,7 @@ module USaidWat
           Dir["#{sr}/*"].each { |f| subreddits[sr] += 1 }
         end
       end
-      subreddits.sort { |a,b| b[1] <=> a[1] }
+      subreddits.sort { |a,b| a[0] <=> b[0] }
     end
     
     def comments_for_subreddit(subreddit)
