df8b1d2a112de09e562d69f7b823bb021e1f1afe
Michael Dippery <michael@monkey-robot.com>
2012-11-26 02:10:25 -0500

    Blow away old source code
    The codebase was becoming a bit of a mess, so I'm going to rewrite the
    gem from scratch for v0.1 using proper BDD practices.

--- lib/usaidwat.rb
+++
@@ -1,7 +0,0 @@
-require 'usaidwat/version'
-require 'usaidwat/reddit'
-
-
-module USaidWat
-  BASE_CACHE_DIR = File.join File.expand_path('~'), '.usaidwat'
-end
--- spec/utils_spec.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
-require 'usaidwat/utils'
-
-describe 'USaidWat::Array' do
-  it 'should return the longest string from a list of tuples' do
-    list = [['Short Name', 0], ['Really Long Name', 1], ['Medium Name', 2]]
-    list.longest_subreddit.should == 'Really Long Name'
-  end
-end
