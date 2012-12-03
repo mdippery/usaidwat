c07d130df2d32af49fb092a017a412a20324241f
Michael Dippery <michael@monkey-robot.com>
2012-12-03 15:36:17 -0500

    Remove use of WebMock in Cucumber tests
    WebMock isn't going to work for Cucumber tests, since Aruba runs the
    usaidwat binary in a separate process, and WebMock doesn't handle URLs
    across processes. Stubbing the method `Snooby.request` _may_ work; we'll
    see. (I might end up having the same problem I was having with WebMock
    vis-à-vis processes.)

--- features/step_definitions/reddit_steps.rb
+++ features/step_definitions/reddit_steps.rb
@@ -1,5 +1,2 @@
 Given /^the Reddit service returns comments for the user "(.*?)"$/ do |user|
-  root = File.expand_path("../../fixtures", __FILE__)
-  stub_request(:get, "http://www.reddit.com/user/#{user}/comments.json?after=&limit=100").
-    to_return(:body => IO.read(File.join(root, "comments.json")))
 end
--- features/support/webmock.rb
+++
@@ -1,6 +0,0 @@
-require 'webmock/cucumber'
-
-Before do
-  WebMock.disable_net_connect!
-  WebMock.reset!
-end
