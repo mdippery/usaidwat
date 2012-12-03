a4281a0d0a95b297586e05f0eeefb5e7cf04c05f
Michael Dippery <michael@monkey-robot.com>
2012-12-02 21:56:49 -0500

    Fix web request mocks
    The snooby gem (which I am using to interface with Reddit) uses different
    URLs than I was expecting to get comments.

--- spec/usaidwat/client_spec.rb
+++ spec/usaidwat/client_spec.rb
@@ -5,15 +5,8 @@ module USaidWat
     describe Client do
       before(:each) do
         root = File.expand_path("../../../test/responses", __FILE__)
-
-        stub_request(:get, "http://www.reddit.com/user/mipadi/comments.json").
-          to_return(:body => IO.read(File.join(root, "comments_1.json")))
-        stub_request(:get, "http://www.reddit.com/user/mipadi/comments.json?count=25&after=t1_c77kq1t").
-          to_return(:body => IO.read(File.join(root, "comments_2.json")))
-        stub_request(:get, "http://www.reddit.com/user/mipadi/comments.json?count=50&after=t1_c74e76h").
-          to_return(:body => IO.read(File.join(root, "comments_3.json")))
-        stub_request(:get, "http://www.reddit.com/user/mipadi/comments.json?count=75&after=t1_c73pvjp").
-          to_return(:body => IO.read(File.join(root, "comments_4.json")))
+        stub_request(:get, "http://www.reddit.com/user/mipadi/comments.json?after=&limit=100").
+          to_return(:body => IO.read(File.join(root, "comments.json")))
       end
       
       let(:redditor) { Client::Redditor.new("mipadi") }
