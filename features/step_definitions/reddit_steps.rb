d139a2680c91eb4c5e6129ae1e2e2ff775938a22
Michael Dippery <michael@monkey-robot.com>
2012-12-02 20:56:40 -0500

    Stub out web requests to Reddit

--- features/step_definitions/reddit_steps.rb
+++ features/step_definitions/reddit_steps.rb
@@ -1,3 +1,12 @@
 Given /^the Reddit service returns comments for the user "(.*?)"$/ do |user|
-  pending
+  ROOT = File.expand_path("../../../test/responses", __FILE__)
+  
+  stub_request(:get, "http://www.reddit.com/user/#{user}/comments.json").
+    to_return(:body => IO.read(File.join(ROOT, "comments_1.json")))
+  stub_request(:get, "http://www.reddit.com/user/#{user}/comments.json?count=25&after=t1_c77kq1t").
+    to_return(:body => IO.read(File.join(ROOT, "comments_2.json")))
+  stub_request(:get, "http://www.reddit.com/user/#{user}/comments.json?count=50&after=t1_c74e76h").
+    to_return(:body => IO.read(File.join(ROOT, "comments_3.json")))
+  stub_request(:get, "http://www.reddit.com/user/#{user}/comments.json?count=75&after=t1_c73pvjp").
+    to_return(:body => IO.read(File.join(ROOT, "comments_4.json")))
 end
