80641003860b3b5e356c2b08d549bf7b00cb1445
Michael Dippery <michael@monkey-robot.com>
2012-12-02 22:17:02 -0500

    Implement feature: Tally subreddits

--- lib/usaidwat/application.rb
+++ lib/usaidwat/application.rb
@@ -8,6 +8,8 @@ module USaidWat
   class Application
     def run(argv)
       @opts, args = handle_arguments(argv)
+      @redditor = USaidWat::Client::Redditor.new(args.first)
+      return tally_comments if @opts[:tally]
     end
     
     def usage(code=0)
@@ -17,6 +19,25 @@ module USaidWat
     
     def version(code=0)
       puts "usaidwat v#{USaidWat::VERSION}"
+    end
+    
+    def tally_comments
+      # Unfortunately Snooby cannot return comments for a specific
+      # user in a specific subreddit, so for now we have to sort them
+      # ourself.
+      longest_subreddit = 0
+      buckets = Hash.new { |hash, key| hash[key] = 0 }
+      @redditor.comments.each do |comment|
+        subreddit = comment.subreddit
+        longest_subreddit = subreddit.length if subreddit.length > longest_subreddit
+        buckets[subreddit] += 1
+      end
+      subreddits = buckets.keys.sort { |a,b| a.downcase <=> b.downcase }
+      subreddits.each do |subreddit|
+        tally = buckets[subreddit]
+        printf "%-*s  %s\n", longest_subreddit, subreddit, tally
+      end
+      exit 0
     end
     
     private
