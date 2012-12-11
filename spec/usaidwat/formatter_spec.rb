bb15709b03d2f30259e95ce5f7119824814f8f91
Michael Dippery <michael@monkey-robot.com>
2012-12-10 20:37:20 -0500

    Test whether horizontal rule is printed properly

--- spec/usaidwat/formatter_spec.rb
+++ spec/usaidwat/formatter_spec.rb
@@ -21,6 +21,24 @@ EXPECTED
           actual = formatter.format(comment)
           actual.should == expected
         end
+        
+        it "should print a horizontal rule for subsequent comments" do
+          expected = "-" * 80
+          comment1 = double("first comment")
+          comment1.should_receive(:subreddit).twice.and_return("programming")
+          comment1.should_receive(:link_id).and_return("t3_13f783")
+          comment1.should_receive(:id).and_return("c73qhxi")
+          comment1.should_receive(:body).and_return("Welcome to the wonderful world of Python drama!")
+          comment2 = double("second comment")
+          comment2.should_receive(:subreddit).twice.and_return("programming")
+          comment2.should_receive(:link_id).and_return("t3_13f783")
+          comment2.should_receive(:id).and_return("c73qhxi")
+          comment2.should_receive(:body).and_return("Welcome to the wonderful world of Python drama!")
+          s = formatter.format(comment1)
+          s = formatter.format(comment2)
+          actual = s.split("\n")[0]
+          actual.should == expected
+        end
       end
     end
   end
