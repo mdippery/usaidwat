a47278ddb5a16e3d7883279cd0afc1a7879e0528
Michael Dippery <michael@monkey-robot.com>
2015-06-23 16:58:47 -0700

    Pluralize strings

--- lib/usaidwat/ext/string.rb
+++ lib/usaidwat/ext/string.rb
@@ -6,4 +6,12 @@ class String
   def convert_entities
     self.gsub(/&gt;/, '>').gsub(/&lt;/, '<')
   end
+
+  def pluralize(n, suffix = 's', singular_suffix = '')
+    if n == 1
+      self + singular_suffix
+    else
+      self + suffix
+    end
+  end
 end
--- spec/usaidwat/string_spec.rb
+++ spec/usaidwat/string_spec.rb
@@ -25,6 +25,48 @@ module USaidWat
           expect(actual).to eq(expected)
         end
       end
+
+      describe "#truncate" do
+        it "truncates strings longer than the given width to the width" do
+          n = 80
+          s = "-" * n * 2
+          expect(s.truncate(n).length).to eq(n)
+        end
+
+        it "does not change strings shorter than the given width" do
+          n = 80
+          s = "string"
+          expect(s.truncate(n).length).to eq(s.length)
+          expect(s.truncate(n).length).to be < n
+        end
+      end
+
+      describe "#pluralize" do
+        it "should not append an 's' to a singular noun" do
+          s = "dog"
+          expect(s.pluralize(1)).to eq(s)
+        end
+
+        it "should append an 's' to a plural noun" do
+          s = "dog"
+          expect(s.pluralize(2)).to eq("dogs")
+        end
+
+        it "should append an 's' to a noun with count 0" do
+          s = "dog"
+          expect(s.pluralize(0)).to eq("dogs")
+        end
+
+        it "should append 'ies' to a plural noun" do
+          s = "popp"
+          expect(s.pluralize(2, "ies", "y")).to eq("poppies")
+        end
+
+        it "should not append 'ies' to a singular noun" do
+          s = "popp"
+          expect(s.pluralize(1, "ies", "y")).to eq("poppy")
+        end
+      end
     end
   end
 end
