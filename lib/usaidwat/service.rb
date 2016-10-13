27b7f91e14b4168feeeaf4eb2a63995bf480988a
Michael Dippery <michael@monkey-robot.com>
2016-10-13 11:46:21 -0700

    Make user agent a class constant

--- lib/usaidwat/service.rb
+++ lib/usaidwat/service.rb
@@ -3,11 +3,13 @@ require 'unirest'
 module USaidWat
   module Service
     class RedditService
+      USER_AGENT = "usaidwat v#{USaidWat::VERSION}"
+
       def user(username)
         data = {}
         %w{about comments submitted}.each do |page|
           url = "https://www.reddit.com/user/#{username}/#{page}.json"
-          hdrs = {'User-Agent' => "usaidwat v#{USaidWat::VERSION}"}
+          hdrs = {'User-Agent' => USER_AGENT}
           r = Unirest.get(url, :headers => hdrs)
           data[page.to_sym] = case r.code
                               when 404 then :no_such_user
