7674ec4ddf1511cb1ebf4ff5573c6e552795fd68
Michael Dippery <michael@monkey-robot.com>
2017-03-01 10:19:12 -0800

    Join arrays in a human-readable way

--- lib/usaidwat/ext/array.rb
+++ lib/usaidwat/ext/array.rb
@@ -2,4 +2,19 @@ class Array
   def subreddits
     join(' ').split(/[ ,\+]/).map { |sr| sr.downcase }
   end
+
+  def or_join
+    case count
+    when 0
+      ''
+    when 1
+      first
+    when 2
+      join(' or ')
+    else
+      last_entry = 'or ' + last
+      items = self[0...-1] << last_entry
+      items.join(', ')
+    end
+  end
 end
--- spec/usaidwat/array_spec.rb
+++ spec/usaidwat/array_spec.rb
@@ -4,32 +4,56 @@ require 'usaidwat/ext/array'
 module USaidWat
   module Ext
     describe Array do
-      it "should split args separated by spaces" do
-        v = %W{one two three}
-        expected = v
-        actual = v.subreddits
-        expect(actual).to eq(expected)
+      describe "#subreddits" do
+        it "should split args separated by spaces" do
+          v = %W{one two three}
+          expected = v
+          actual = v.subreddits
+          expect(actual).to eq(expected)
+        end
+
+        it "should split args separated by commas" do
+          v = ['one,two,three']
+          expected = %W{one two three}
+          actual = v.subreddits
+          expect(actual).to eq(expected)
+        end
+
+        it "should split args separated by plus signs" do
+          v = ['one+two+three']
+          expected = %W{one two three}
+          actual = v.subreddits
+          expect(actual).to eq(expected)
+        end
+
+        it "should downcase all args" do
+          v = %W{one Two Three}
+          expected = %W{one two three}
+          actual = v.subreddits
+          expect(actual).to eq(expected)
+        end
       end
 
-      it "should split args separated by commas" do
-        v = ['one,two,three']
-        expected = %W{one two three}
-        actual = v.subreddits
-        expect(actual).to eq(expected)
-      end
+      describe "#or_join" do
+        it "should return the first item if the array has one item" do
+          v = ['wikipedia']
+          expect(v.or_join).to eq("wikipedia")
+        end
 
-      it "should split args separated by plus signs" do
-        v = ['one+two+three']
-        expected = %W{one two three}
-        actual = v.subreddits
-        expect(actual).to eq(expected)
-      end
+        it "should separate the last entry by 'or'" do
+          v = ['wikipedia', 'programming']
+          expect(v.or_join).to eq('wikipedia or programming')
+        end
 
-      it "should downcase all args" do
-        v = %W{one Two Three}
-        expected = %W{one two three}
-        actual = v.subreddits
-        expect(actual).to eq(expected)
+        it "should separate all entries with commas and add the word 'or'" do
+          v = ['wikipedia', 'programming', 'AskReddit']
+          expect(v.or_join).to eq('wikipedia, programming, or AskReddit')
+        end
+
+        it "should return an empty string for empty arrays" do
+          v = []
+          expect(v.or_join).to eq('')
+        end
       end
     end
   end
